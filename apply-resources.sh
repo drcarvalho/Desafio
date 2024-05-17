7b1d702ec56d692908e1bb375343acc425ba7bdf
Daniel Rodrigues <daniel.rodrigues14@icloud.com>
2024-05-17 04:16:37 -0300

    adicionei um wait no script e alterei o arquivo de configurar etc

--- apply-resources.sh
+++ apply-resources.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Aplica o arquivo de manifesto para implantar os recursos no cluster
+# Aplica o arquivo de manifesto no cluster
 kubectl apply -f kubernetes/deployment.yaml
 kubectl apply -f kubernetes/services.yaml
-# Adicione mais comandos kubectl apply conforme necessário
+
+echo "Aguardando 30 segundos para o pod e o service iniciar..."
+sleep 30
+
+# Configura o redirecionamento de porta para usar localhost
+kubectl port-forward service/services-desafio-globo 8000:80 &
---
+++ configure-etc-hosts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Adiciona a entrada de host ao arquivo /etc/hosts
+sudo sh -c "echo '127.0.0.1 comments.devops-challenge.globo.local' >> /etc/hosts"
